540ffa5e068f1ef7676a60a77b6c0de24905a6a9
Luciano <lucianodefranca@outlook.com>
2024-02-01 20:38:56 -0300

    oh boy this is getting weird

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::os::windows;
-
 use ::windows::core::Interface;
+use std ::{ffi::OsString, os::windows::ffi::OsStringExt}; //std são standard do Rust e não precisam ser includias no cargo.toml
 use windows::{
     core::{GUID,HSTRING,PCWSTR,PWSTR},
     Win32::{
@@ -13,39 +13,40 @@ use windows::{
     },
 };
 
-fn open_wlan_handle(api_version: u32) -> Result<HANDLE, windows::core::Error> {
+fn open_wlan_handle(api_version: u32) -> Result<HANDLE, windows::core::Error> { //Abre uma conexão com o sistema de rede wireless, 
     let mut negotiated_vesion:i32 = 0;
     let mut wlan_handle = INVALID_HANDLE_VALUE;
-
     let result:() = unsafe {WlanOpenHandle(api_version, None, &mut negotiated_vesion, &mut wlan_handle)};
 
     WIN32_ERROR(result).ok()?;
     Ok(wlan_handle)
     }
 
-fn enum_wlan_interfaces(handle :HANDLE) -> Result<*mut WLAN_INTERFACE_INFO_LIST, windows::core::Error>{
-
+fn enum_wlan_interfaces(handle :HANDLE) -> Result<*mut WLAN_INTERFACE_INFO_LIST, windows::core::Error>{ //Enumera as redes disponiveis
     let mut interface_ptr = std::ptr::null_mut();
-
     let result = unsafe { WLanEnumInterfaces(handle, None, &mut interface_ptr)};
 
     WIN32_ERROR(result).ok()?;
-
     Ok(interface_ptr)
 
 }
-fn grab_interface_profiles(
+fn grab_interface_profiles( //Pega ouma lista dos profile names de redes wireless no sistema pelo GUID
     handle :HANDLE,
     interface_guid: &GUID
 )-> Result<*const WLAN_PROFILE_INFO_LIST, windows::core::Error>{
     let mut wlan_profiles_ptr = std::ptr::null_mut();
-
     let result = unsafe {WlanGetProfileList (handle, interface_guid, None, &mut wlan_profiles_ptr)};
     WIN32_ERROR(result).ok()?;
 
     Ok(wlan_profiles_ptr)
 
 }
+
+fn parse_utf16_slice(string_slice : &[u16] ) -> Option<OsString>{
+    let null_index = string_slice.iter().position(|c| c == &0 )?;
+}
+
+
 
 fn main() {
 
