2cbbd9308a6481e13a9f24f8acf8b72d899649ab
Luciano <lucianodefranca@outlook.com>
2024-02-01 20:42:18 -0300

    oh boy this is getting weird

--- src/main.rs
+++ src/main.rs
@@ -44,6 +44,8 @@ fn grab_interface_profiles( //Pega ouma lista dos profile names de redes wireles
 
 fn parse_utf16_slice(string_slice : &[u16] ) -> Option<OsString>{
     let null_index = string_slice.iter().position(|c| c == &0 )?;
+
+    Some(OsString::from_wide(&string_slice[..null_index]))
 }
 
 
